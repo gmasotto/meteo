815bf648ee7ca54300eb84e813b04f2be374c8a7
Giovanni Masotto <giooo6959@gmail.com>
2026-02-20 11:50:15 +0100

    fix logo in the header

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,14 +1,17 @@
 import ThemeToggle from "@/components/ThemeToggle";
-import { CloudSun } from "lucide-react";
 import { type ReactNode } from "react";
 
 function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-background p-4">
-      <header className="border-b ">
+      <header className="border-b">
         <div className="flex h-16 items-center justify-between">
           <div className="flex items-center gap-3 font-semibold">
-            <CloudSun className="h-6 w-6 text-primary" />
+            <img
+              src="/favicon.svg"
+              alt="Meteo logo"
+              className="h-8 w-8 rounded-xl"
+            />
             <p>Meteo Dashboard</p>
           </div>
           <ThemeToggle />
